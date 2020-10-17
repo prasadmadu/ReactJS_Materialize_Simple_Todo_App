61fe98bf1f60622402850d87d78c74b12585cfb0
prasad <ncprasadmadusanka@gmail.com>
2020-10-17 17:07:56 +0530

    Updates

---
+++ precache-manifest.d04f80de5b29e6c7ed29427c32880b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c48fdd04e75a626705c5bad7e2af9d8",
+    "url": "/ReactJS_Materialize_Simple_Todo_App/index.html"
+  },
+  {
+    "revision": "5ac02f32275695f06ae3",
+    "url": "/ReactJS_Materialize_Simple_Todo_App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f4cc85596e59456bb89",
+    "url": "/ReactJS_Materialize_Simple_Todo_App/static/js/2.0c71bd63.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ReactJS_Materialize_Simple_Todo_App/static/js/2.0c71bd63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac02f32275695f06ae3",
+    "url": "/ReactJS_Materialize_Simple_Todo_App/static/js/main.58164c80.chunk.js"
+  },
+  {
+    "revision": "f4c339b2786007bb53f8",
+    "url": "/ReactJS_Materialize_Simple_Todo_App/static/js/runtime-main.20316d99.js"
+  }
+]);
